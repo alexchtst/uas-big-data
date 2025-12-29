29f692ee43614ea2caaed2f5705ef2751567fce3
alexchtst <acinatra@gmail.com>
2025-12-29 18:58:04 +0700

    feat: edit the runner bash script

--- hadoop-runner.sh
+++ hadoop-runner.sh
@@ -1,7 +1,11 @@
+hdfs dfs -rm -r /data/output
+
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming*.jar \
-  -input '/home/ach/ach-codespace/uas-bigdata/data/E-commerce Dataset.csv' \
-  -output /home/ach/ach-codespace/uas-bigdata/output \
+  -input '/data/input/e_commerce_dataset.csv' \
+  -output /data/output \
   -mapper mapper-productcategory.py \
   -reducer reducer-productcategory.py \
   -file mapper-productcategory.py \
   -file reducer-productcategory.py
+
+hdfs dfs -get /data/output/part-00000 ./output/hasil_mapreduce.txt
